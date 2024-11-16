6fdd88386d4ee5a3c137c23de9dbcd7a8e79b660
Skywalker003 <srivikasramadas@gmail.com>
2024-11-16 13:40:48 +0530

    added

--- javascript/variables/script.js
+++ javascript/variables/script.js
@@ -1,6 +1,13 @@
-var price = 20
+/*var price = 20
 var product = "Apple"
 var tax = 4
 console.log(product);
 total = tax + price
-console.log(total);
+console.log(total);*/
+
+var FruitName = "orange"
+var count = 5
+var price = 10
+var total = count * price
+console.log(FruitName)
+console.log(total)
