e57037f1d5fa20aeebd70be776d57e843e6bd873
Skywalker003 <srivikasramadas@gmail.com>
2024-11-16 18:01:48 +0530

    added

--- javascript/functionss/script.js
+++ javascript/functionss/script.js
@@ -1,4 +1,59 @@
+/*
 function hi(){
     console.log("hello world")
 }
 hi()
+*/
+
+/*
+function check(){
+    console.log("function is working")
+}
+check()
+*/
+
+/*
+function Phone(){
+    console.log("this is iphone")
+    console.log("this is redmi")
+    console.log("this is oppo")
+}
+Phone()
+*/
+
+/*
+function redmi(){
+    console.log("this is redmi note 13 pro")
+}
+function apple(){
+    console.log("this is apple 16 pro max")
+}
+function oppo(){
+    console.log("this is oppo find x8")
+}
+
+redmi()
+apple()
+oppo()
+*/
+
+/*exercise 1 :
+var a = 10
+var b = 20
+
+function add(){
+    console.log(a+b)
+}
+add()
+*/
+
+//exercise 2 :
+var factor = "vijay"
+var fplayer = "dhoni"
+var fmovie = "theri"
+function favourite(){
+    console.log("favourite actor: " + factor)
+    console.log("favourite player: " + fplayer)
+    console.log("favourite movie: " + fmovie)
+}
+favourite()
