cb1eaa4dcc44f7d91876cc92ebb677aa1529f196
Skywalker003 <srivikasramadas@gmail.com>
2024-11-16 21:02:21 +0530

    added

---
+++ javascript/if else/script.js
@@ -0,0 +1,15 @@
+/*
+var weather = "raining"
+if(weather = "raining" ){
+    console.log("take an umbrella")
+}else{
+    console.log("enjoy the sun shine")
+}
+*/
+
+var rain = true
+if(rain){
+    console.log("take an umbrella")
+}else{
+    console.log("enjoy the sun shine")
+}
---
+++ javascript/return/script.js
@@ -0,0 +1,14 @@
+/*
+function myname(){
+    return "srivikas";
+}
+
+var a = myname();
+console.log(a)
+*/
+
+function add(a,b){
+    return a+b;
+}
+var sum = add(5,10)
+console.log(sum)
