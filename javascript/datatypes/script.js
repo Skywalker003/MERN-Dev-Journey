276a5089039cd3e06449b41480afa913108bf632
Skywalker003 <srivikasramadas@gmail.com>
2024-11-16 13:51:32 +0530

    added

---
+++ javascript/datatypes/script.js
@@ -0,0 +1,29 @@
+var FruitName = "orange"
+var count = 5
+var price = 10
+var total = count * price
+console.log(FruitName)
+console.log(total)
+
+console.log(typeof(FruitName))
+console.log(typeof(total)) 
+
+
+ /*
+ there are 2 types of data 
+ one is primitive data type
+ other is non-primitive data type
+
+
+ primitive:
+ num
+ string
+ boolean
+ null
+ undefined
+
+ non-primitive:
+ array
+ object
+
+*/
---
+++ javascript/operators/script.js
@@ -0,0 +1,6 @@
+var FruitName = "orange"
+var count = 5
+var price = 10
+var total = count * price
+console.log(FruitName)
+console.log(total)
