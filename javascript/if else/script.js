7acc47e41fd09a9c90ece77a46f6183e873144b4
Skywalker003 <srivikasramadas@gmail.com>
2024-11-17 12:10:25 +0530

    added

--- javascript/if else/script.js
+++ javascript/if else/script.js
@@ -1,15 +1,108 @@
 /*
 var weather = "raining"
-if(weather = "raining" ){
+if(weather == "raining" ){
     console.log("take an umbrella")
 }else{
     console.log("enjoy the sun shine")
 }
 */
 
+// exercise 1
+/*
 var rain = true
 if(rain){
     console.log("take an umbrella")
 }else{
     console.log("enjoy the sun shine")
+}
+*/
+
+// exercise 2
+/*
+var hw = true
+if(hw){
+    console.log("great job!")
+}else{
+    console.log("finish ur homework before playing")
+}
+*/
+
+// exercise 3
+/*
+var cookies = false
+if(cookies){
+    console.log("would u like a cookie")
+}else{
+    console.log("time to bake more cookies")
+}
+*/
+
+//---------------------------------------------------------
+
+//exercise 1
+/*
+var signal = "green"
+if(signal == "red"){
+    console.log("stop")
+}
+if(signal == "yellow"){
+    console.log("get ready")
+}
+if(signal == "green" ){
+    console.log("go")
+}
+*/
+
+//exercise 2
+/*
+var season ="winter"
+if(season == "spring"){
+    console.log("enjoy the blooming flowers")
+}
+if(season == "summer"){
+    console.log("have fun in the sun")
+}
+if(season == "autumn"){
+    console.log("admire the colorful leaves")
+}
+if(season == "winter"){
+    console.log("bundle up and stay warm")
+}
+*/
+
+//---------------------------------------------------------
+
+//exercise 1
+/*
+var score = 65
+if(score<50){
+    console.log("you need to improve")
+}
+else if(score>=50 && score<=70){
+    console.log("good job!")
+}
+else if(score>70){
+    console.log("excellent performance")
+}
+*/
+
+//exercise 2
+/*
+var num = "odd"
+if(num == "odd"){
+    console.log("the number is odd")
+}else{
+    console.log("the number is even")
+}
+*/
+
+//exercise 3
+
+var char = "h";
+char = char.toLowerCase();
+
+if("aeiou".includes(char)){
+    console.log("it is a vowel")
+}else{
+    console.log("it is a consonant")
 }
---
+++ javascript/logical op/script.js
@@ -0,0 +1,18 @@
+/*
+AND
+both should be true else false
+console.log(true && false)
+*/
+
+/*
+OR
+at least one should be true else false
+console.log(true || false) 
+*/
+
+/*
+NOT
+it will return the opposite of the value
+console.log(!true)
+*/
+
