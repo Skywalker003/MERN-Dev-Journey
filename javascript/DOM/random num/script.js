67ec552100fb0c0ff2c1fbad101c008164af12f0
Skywalker003 <srivikasramadas@gmail.com>
2024-11-17 19:57:39 +0530

    added

---
+++ javascript/DOM/random num/script.js
@@ -0,0 +1,4 @@
+var a = Math.random()
+console.log(Math.floor(a*10)+1) //we are *10 so it gives 0 to 9 and +1 so it gives 1 to 10
+//math.random give a random number between 0 and 0.9
+//math.floor round down the num
