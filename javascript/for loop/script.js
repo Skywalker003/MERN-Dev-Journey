b01448951b981ada0151dd9f8f8079853d191855
Skywalker003 <srivikasramadas@gmail.com>
2024-11-17 13:16:55 +0530

    added

---
+++ javascript/for loop/script.js
@@ -0,0 +1,32 @@
+/*for(count=1;count<=10;count++){
+    console.log("sky walker")
+}
+*/
+
+/*
+1 to 10
+for(count=1;count<=10;count++){
+    console.log(count)
+}
+*/
+
+/*
+1 to 10 but increment by 2
+for(i=1;i<=10;i=i+2){
+    console.log(i)
+}
+*/
+
+/*
+print 1 to 10 in reverse order
+for(i=10;i>=1;i--){
+    console.log(i)
+}
+*/
+
+/*
+2 tables
+for(i=1;i<=10;i++){
+    console.log(i+" X 2 = " + i*2)
+}
+*/
