70922e3502e06f4821bbdbac1d948408b715e9b8
Skywalker003 <srivikasramadas@gmail.com>
2024-11-19 21:59:22 +0530

    added

---
+++ javascript/student form/script.js
@@ -0,0 +1,12 @@
+function display(){
+//selecting and getting elements
+var name = document.getElementById("name")
+var age = document.getElementById("age")
+var gender = document.querySelector('input[name="gender"]:checked')
+var course = document.getElementById("course")
+var email = document.getElementById("email")
+
+//creating tr
+
+
+}
