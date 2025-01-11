1698c976689abb726989dca00fb61ec7ed7e1e35
Skywalker003 <srivikasramadas@gmail.com>
2025-01-11 13:31:49 +0530

    added

---
+++ react/a_demo/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Heading() {
+  return (
+    <h1>This is an h1 heading</h1>
+  )
+}
+
+function App() {
+  return ( 
+    <div className="App"> 
+      This is the starting code for "Your first component" ungraded lab 
+      <Heading /> 
+    </div> 
+  ); 
+}
+
+
+ 
+export default App;
---
+++ react/a_demo/src/components/Nav.js
@@ -0,0 +1,10 @@
+function Nav(){
+    return (
+        <nav class = "main-nav">
+            <ul>
+                
+            </ul>
+        </nav>
+        
+    )
+}
