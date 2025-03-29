34a5f01f4048b98aacc0bd607bbedf53a2cedaec
SRI VIKAS S R <srivikasramadas@gmail.com>
2025-03-29 13:55:58 +0530

    Add files via upload

---
+++ react/react_fun_fact_pg/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+    return (
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul className="facts-list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200K stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
---
+++ react/react_fun_fact_pg/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src="/images/react-logo.png" alt="React logo" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
