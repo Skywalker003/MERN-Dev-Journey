1860c3eebbb58ec622047d520bbf8599878274cb
Skywalker003 <srivikasramadas@gmail.com>
2025-12-29 21:17:44 +0530

    added

--- react/recap/course_site/src/components/Card.jsx
+++ react/recap/course_site/src/components/Card.jsx
@@ -1,24 +1,29 @@
 export default function Card(props){
-    if(props.shown==true){
-        return(
-            <div className="card"> {/* can use props.shown&& if there is no else part */}
-                <img className="inner-img" src={props.image} alt="img" />
-                <div className="inner-text">
-                    <h3>{props.title}</h3>
-                    <p>{props.description}</p>
-                    <p>{props.difficulty}</p>
-                    <p>{props.level}</p>
-                </div> 
-            </div>
-        )
-    }
-    else{
-        return(
-            <div className="card">
-                <h1 className="update">This course is coming soon!</h1>
-            </div>
-        )
-    }
+  function handleClick(){
+      alert(`You have enrolled in ${props.title} course!`)
+      console.log(`Enrolled in ${props.title} course`)
+  }
+  if(props.shown==true){
+      return(
+          <div className="card"> {/* can use props.shown&& if there is no else part */}
+              <img className="inner-img" src={props.image} alt="img" />
+              <div className="inner-text">
+                  <h3>{props.title}</h3>
+                  <p>{props.description}</p>
+                  <p>{props.difficulty}</p>
+                  <p>{props.level}</p>
+                  <button onClick={handleClick}>Enroll Now</button>
+              </div> 
+          </div>
+      )
+  }
+  else{
+      return(
+          <div className="card">
+              <h1 className="update">This course is coming soon!</h1>
+          </div>
+      )
+  }
 }
 
 /*
