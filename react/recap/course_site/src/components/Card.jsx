33d4b46bd71fb2ebba9c9e4b65bc62a939be2f2e
Skywalker003 <srivikasramadas@gmail.com>
2025-12-29 15:05:49 +0530

    added

--- react/recap/course_site/src/components/Card.jsx
+++ react/recap/course_site/src/components/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card(props){
     if(props.shown==true){
         return(
-            <div className="card">
+            <div className="card"> {/* can use props.shown&& if there is no else part */}
                 <img className="inner-img" src={props.image} alt="img" />
                 <div className="inner-text">
                     <h3>{props.title}</h3>
@@ -20,3 +20,24 @@ export default function Card(props){
         )
     }
 }
+
+/*
+export default function Card(props) {
+  return (
+    <div className="card">
+      {props.shown ? (
+        <>
+          <img className="inner-img" src={props.image} alt="img" />
+          <div className="inner-text">
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+            <p>{props.difficulty}</p>
+            <p>{props.level}</p>
+          </div>
+        </>
+      ) : (
+        <h1 className="update">This course is coming soon!</h1>
+      )}
+    </div>
+  );
+}*/
--- react/recap/course_site/src/components/Course.jsx
+++ react/recap/course_site/src/components/Course.jsx
@@ -10,12 +10,23 @@ import { courses } from "../data/coursesData";
 import Card from "./Card";
 
 export default function Course(){
+  const levelOrder = {
+  Beginner: 1,
+  "Beginner → Intermediate": 2,
+  Intermediate: 3,
+  Advanced: 4,
+};
   return(
     <div className="card-container">
-      {courses.map(
-        ({ id, title, image, description, difficulty, level, shown }) => ( //destructured instead of (course) and then course.id, course.title etc
+      {[...courses]
+      .sort(
+        (a, b) => levelOrder[a.level] - levelOrder[b.level]
+      )
+      .filter((course) => levelOrder[course.level] <= levelOrder["Intermediate"])
+      .map(
+        ({ id, title, image, description, difficulty, level, shown } /*, index*/) => ( //destructured instead of (course) and then course.id, course.title etc
           <Card
-            key={id}
+            key={id}// index can also be used as key but id is better
             title={title}
             image={image}
             description={description}
